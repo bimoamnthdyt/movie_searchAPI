f247d8ba0dcd2e3eedaa23534efb70c8d32175d1
bimoamnthdyt <bimoamanta9@gmail.com>
2020-11-27 10:37:16 +0700

    row film

--- js/script.js
+++ js/script.js
@@ -1,18 +1,38 @@
 $('#search-button').on('click', function () {
     $.ajax({
-        url: 'http://www.omdbapi.co/',
+        url: 'http://www.omdbapi.com/',
         type: 'get',
         dataType: 'json',
         data: {
             'apikey' : 'f7ee9825',
-            's' : '',
+            's' : $('#search-input').val()
         },
         success : function (result) {
-            if (result.Response == "true"){
-                let movies = result.Search;
-                console.log(movies);
+            if (result.Response == "True"){
+                let movies = result.Search
+                
+                $.each(movies, function (i, data) {
+                    $('#movie-list').append(`
+                    <div class="col-md-4"> 
+                    <div class="card">
+                        <img src="` + data.Poster +`" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                    </div>
+                    `);
+                });
+
             }else{
-                $('#movie-list').html('<h1>Movie Not Found!</h1>')
+                $('#movie-list').html(`
+                <div class='col'>
+                    <h1 class = "text-center">`+ result.Error +`</h1>
+                </div>
+                `);
+                
             }
         }
     });
